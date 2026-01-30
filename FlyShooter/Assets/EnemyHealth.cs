544481e8864e033e5f89161531e014fab56bdf3c
ducviet205 <buiducvietdz2005@gmail.com>
2026-01-30 09:36:36 +0700

    demo part 3

---
+++ FlyShooter/Assets/EnemyHealth.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public GameObject explosionPrefab; // <-- field này
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        // Tạo vụ nổ tại vị trí enemy
+        if (explosionPrefab != null)
+        {
+            GameObject explosion = Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            // Tự hủy sau 1 giây (thời gian animation)
+            Destroy(explosion, 1f);
+        }
+
+        Destroy(gameObject); // Xóa enemy
+    }
+}
--- FlyShooter/Assets/Space Shooter Template FREE/Scripts/PlayerShooting.cs
+++ FlyShooter/Assets/Space Shooter Template FREE/Scripts/PlayerShooting.cs
@@ -1,47 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
-
-//guns objects in 'Player's' hierarchy
+// Guns objects in 'Player's' hierarchy
 [System.Serializable]
 public class Guns
 {
-    public GameObject  centralGun;
-    [HideInInspector] public ParticleSystem centralGunVFX; 
+    public GameObject centralGun;
+    [HideInInspector] public ParticleSystem centralGunVFX;
 }
 
-public class PlayerShooting : MonoBehaviour {
-
+[DebuggerDisplay("{" + nameof(GetDebuggerDisplay) + "(),nq}")]
+public class PlayerShooting : MonoBehaviour
+{
     [Tooltip("shooting frequency. the higher the more frequent")]
     public float fireRate;
 
     [Tooltip("projectile prefab")]
     public GameObject projectileObject;
 
-    //time for a new shot
+    // Time for next shot
     [HideInInspector] public float nextFire;
 
-
     [Tooltip("current weapon power")]
-    [Range(1, 4)]       //change it if you wish
-    public int weaponPower = 1; 
+    [Range(1, 4)]
+    public int weaponPower = 1;
 
     public Guns guns;
-    bool shootingIsActive = true; 
-    [HideInInspector] public int maxweaponPower = 4; 
+
+    bool shootingIsActive = true;
+    [HideInInspector] public int maxweaponPower = 4;
+
     public static PlayerShooting instance;
+
+    [Header("Bullet Spawn Settings")]
+    [Tooltip("Offset from central gun position (e.g., forward to nose tip)")]
+    public Vector3 bulletOffset = new Vector3(0, 0.7f, 0);  // Default Y+0.7 như tutorial slide
+
+    [Header("Multi-shot Settings")]
+    public float spreadAngle = 15f;  // Góc lệch cho level cao (độ)
 
     private void Awake()
     {
         if (instance == null)
             instance = this;
     }
+
     private void Start()
     {
-        //receiving shooting visual effects components
-        
-        guns.centralGunVFX = guns.centralGun.GetComponent<ParticleSystem>();
+        // Receiving shooting visual effects components
+        if (guns.centralGun != null)
+        {
+            guns.centralGunVFX = guns.centralGun.GetComponent<ParticleSystem>();
+        }
     }
 
     private void Update()
@@ -50,35 +62,67 @@ public class PlayerShooting : MonoBehaviour {
         {
             if (Time.time > nextFire)
             {
-                MakeAShot();                                                         
+                MakeAShot();
                 nextFire = Time.time + 1 / fireRate;
             }
         }
     }
 
-    //method for a shot
-    void MakeAShot() 
+    // Method for a shot
+    void MakeAShot()
     {
-        switch (weaponPower) // according to weapon power 'pooling' the defined anount of projectiles, on the defined position, in the defined rotation
+        // Luôn play VFX ở central gun nếu có
+        if (guns.centralGunVFX != null)
+            guns.centralGunVFX.Play();
+
+        // Tính vị trí base spawn (centralGun + offset)
+        Vector3 spawnBasePos = guns.centralGun != null
+            ? guns.centralGun.transform.position + bulletOffset
+            : transform.position + bulletOffset;
+
+        switch (weaponPower)
         {
             case 1:
-                CreateLazerShot(projectileObject, guns.centralGun.transform.position, Vector3.zero);
-                guns.centralGunVFX.Play();
+                // Level 1: Chỉ 1 đạn giữa
+                CreateLazerShot(projectileObject, spawnBasePos, Vector3.zero);
                 break;
-            
+
+            case 2:
+                // Level 2: 2 đạn, lệch trái phải
+                CreateLazerShot(projectileObject, spawnBasePos, new Vector3(0, 0, spreadAngle));
+                CreateLazerShot(projectileObject, spawnBasePos, new Vector3(0, 0, -spreadAngle));
+                break;
+
             case 3:
-                CreateLazerShot(projectileObject, guns.centralGun.transform.position, Vector3.zero);
-               
+                // Level 3: 3 đạn (giữa + lệch)
+                CreateLazerShot(projectileObject, spawnBasePos, Vector3.zero);
+                CreateLazerShot(projectileObject, spawnBasePos, new Vector3(0, 0, spreadAngle));
+                CreateLazerShot(projectileObject, spawnBasePos, new Vector3(0, 0, -spreadAngle));
                 break;
+
             case 4:
-                CreateLazerShot(projectileObject, guns.centralGun.transform.position, Vector3.zero);
-                
+                // Level 4: 5 đạn, spread rộng
+                CreateLazerShot(projectileObject, spawnBasePos, new Vector3(0, 0, spreadAngle * 1.5f));
+                CreateLazerShot(projectileObject, spawnBasePos, new Vector3(0, 0, spreadAngle * 0.5f));
+                CreateLazerShot(projectileObject, spawnBasePos, Vector3.zero);
+                CreateLazerShot(projectileObject, spawnBasePos, new Vector3(0, 0, -spreadAngle * 0.5f));
+                CreateLazerShot(projectileObject, spawnBasePos, new Vector3(0, 0, -spreadAngle * 1.5f));
+                break;
+
+            default:
+                CreateLazerShot(projectileObject, spawnBasePos, Vector3.zero);
                 break;
         }
     }
 
-    void CreateLazerShot(GameObject lazer, Vector3 pos, Vector3 rot) //translating 'pooled' lazer shot to the defined position in the defined rotation
+    void CreateLazerShot(GameObject lazer, Vector3 pos, Vector3 rotEuler)
     {
-        Instantiate(lazer, pos, Quaternion.Euler(rot));
+        Quaternion rot = Quaternion.Euler(rotEuler);
+        Instantiate(lazer, pos, transform.rotation * rot);  // Rotation của tàu + spread angle
+    }
+
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
     }
 }
